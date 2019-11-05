a42d77e8e970457a4ea58f84cb2eec3a7af43ba6
L4v <ivos.jovan@protonmail.ch>
2019-11-05 19:22:47 +0100

    #glavni_prozor Zapocet.

---
+++ src/studentskasluzba/GlavniProzor.java
@@ -0,0 +1,19 @@
+package studentskasluzba;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class GlavniProzor extends JFrame{
+	public GlavniProzor()
+	{
+		super();
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Dimension ScreenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		this.setSize(ScreenSize.width * 3/4, ScreenSize.height * 3/4);
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+	}
+}
---
+++ src/studentskasluzba/Main.java
@@ -0,0 +1,9 @@
+package studentskasluzba;
+
+public class Main {
+	public static void main(String args[])
+	{
+		GlavniProzor Prozor = new GlavniProzor();
+		
+	}
+}
