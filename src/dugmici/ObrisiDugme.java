9528acba5077336f15ce84231d0c8c29879e1aea
L4v <ivos.jovan@protonmail.ch>
2019-11-05 20:27:16 +0100

    #toolbar Dodati search tekst polje i dugme. #glavni_prozor Onesposobljeno menjanje velicine

--- src/dugmici/ObrisiDugme.java
+++ src/dugmici/ObrisiDugme.java
@@ -1,4 +1,4 @@
-package studentskasluzba;
+package dugmici;
 
 import java.awt.Image;
 import java.io.IOException;
@@ -8,12 +8,12 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 @SuppressWarnings("serial")
-public class DodajDugme extends JButton{
-	public DodajDugme()
+public class ObrisiDugme extends JButton{
+	public ObrisiDugme()
 	{
 		super();
 		try {
-			Image Icon = ImageIO.read(getClass().getResource("/icons/add-user.png"));
+			Image Icon = ImageIO.read(getClass().getResource("/icons/delete.png"));
 			this.setIcon(new ImageIcon(Icon));
 		} catch (IOException e) {
 			e.printStackTrace();
--- src/studentskasluzba/Toolbar.java
+++ src/studentskasluzba/Toolbar.java
@@ -1,14 +1,31 @@
 package studentskasluzba;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JTextField;
 import javax.swing.JToolBar;
+
+import dugmici.DodajDugme;
+import dugmici.IzmeniDugme;
+import dugmici.ObrisiDugme;
+import dugmici.PretragaDugme;
 
 @SuppressWarnings("serial")
 public class Toolbar extends JToolBar{
 	public Toolbar()
 	{
 		super();
+		
+		// NOTE(Jovan): Onesposobljava pomeranje
+		this.setFloatable(false);
+		
+		// NOTE(Jovan): Dugmici za Add, Edit, Delete
 		this.add(new DodajDugme());
 		this.add(new IzmeniDugme());
 		this.add(new ObrisiDugme());
+		
+		// NOTE(Jovan): Search polje i dugme
+		this.add(new JTextField(), BorderLayout.EAST);
+		this.add(new PretragaDugme());
 	}
 }
