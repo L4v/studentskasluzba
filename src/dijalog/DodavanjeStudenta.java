b348cbb7536f1cd8460cdd0370abd1690194f03e
Kris98Wolf <57412501+Kris98Wolf@users.noreply.github.com>
2019-12-01 16:57:01 +0100

    #dodavanje_studenta izglancati

--- src/dijalog/DodavanjeStudenta.java
+++ src/dijalog/DodavanjeStudenta.java
@@ -2,8 +2,10 @@ package dijalog;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
@@ -15,6 +17,8 @@ public class DodavanjeStudenta extends JDialog{
 	public DodavanjeStudenta() {
 		
 		setTitle("Dodavanje studenta");
+		setSize(450,500);
+		setLocationRelativeTo(null);
 		
 		GridBagLayout layout = new GridBagLayout();
 		getContentPane().setLayout(layout);
@@ -26,6 +30,9 @@ public class DodavanjeStudenta extends JDialog{
 		JLabel brTelefona = new JLabel("Broj telefona*");
 		JLabel brIndeksa = new JLabel("Broj Indeksa*");
 		JLabel trenutnaGod = new JLabel("Trenutna godina*");
+		
+		JButton potvrdi = new JButton("Potvrdi");
+		JButton otkazi = new JButton("Otka\u017ei");
 		
 		JComboBox trenGod = new JComboBox(new String[] { "I (prva)", "II (druga)", "III (tre\u0107a)", "IV (\u010Detvrta)"});
 			
@@ -46,91 +53,129 @@ public class DodavanjeStudenta extends JDialog{
 		GridBagConstraints c = new GridBagConstraints();
 		c.gridx = 0;
 		c.gridy = 0;
+		c.insets = new Insets(2, 2, 2, 2);
 		c.anchor = GridBagConstraints.WEST;
 		getContentPane().add(ime, c);
 		
 		GridBagConstraints c1 = new GridBagConstraints();
-		c.gridx = 1;
-		c.gridy = 0;
+		c1.gridx = 1;
+		c1.gridy = 0;
+		c1.weightx = 1;
+		c1.insets = new Insets(2, 2, 2, 2);
+		c1.fill = GridBagConstraints.HORIZONTAL;
 		getContentPane().add(imeT, c1);
 		
 		GridBagConstraints c2 = new GridBagConstraints();
-		c.gridx = 0;
-		c.gridy = 1;
-		c.anchor = GridBagConstraints.WEST;
+		c2.gridx = 0;
+		c2.gridy = 1;
+		c2.insets = new Insets(2, 2, 2, 2);
+		c2.anchor = GridBagConstraints.WEST;
 		getContentPane().add(prezime, c2);
 		
 		GridBagConstraints c3 = new GridBagConstraints();
-		c.gridx = 1;
-		c.gridy = 1;
+		c3.gridx = 1;
+		c3.gridy = 1;
+		c3.insets = new Insets(2, 2, 2, 2);
+		c3.fill = GridBagConstraints.HORIZONTAL;
 		getContentPane().add(prezimeT, c3);
 		
 		GridBagConstraints c4 = new GridBagConstraints();
-		c.gridx = 0;
-		c.gridy = 2;
-		c.anchor = GridBagConstraints.WEST;
+		c4.gridx = 0;
+		c4.gridy = 2;
+		c4.insets = new Insets(2, 2, 2, 2);
+		c4.anchor = GridBagConstraints.WEST;
 		getContentPane().add(datRodj, c4);
 		
 		GridBagConstraints c5 = new GridBagConstraints();
-		c.gridx = 1;
-		c.gridy = 2;
+		c5.gridx = 1;
+		c5.gridy = 2;
+		c5.insets = new Insets(2, 2, 2, 2);
+		c5.fill = GridBagConstraints.HORIZONTAL;
 		getContentPane().add(datRodjT, c5);
 		
 		GridBagConstraints c6 = new GridBagConstraints();
-		c.gridx = 0;
-		c.gridy = 3;
-		c.anchor = GridBagConstraints.WEST;
+		c6.gridx = 0;
+		c6.gridy = 3;
+		c6.insets = new Insets(2, 2, 2, 2);
+		c6.anchor = GridBagConstraints.WEST;
 		getContentPane().add(adresa, c6);
 		
 		GridBagConstraints c7 = new GridBagConstraints();
-		c.gridx = 1;
-		c.gridy = 3;
+		c7.gridx = 1;
+		c7.gridy = 3;
+		c7.insets = new Insets(2, 2, 2, 2);
+		c7.fill = GridBagConstraints.HORIZONTAL;
 		getContentPane().add(adresaT, c7);
 		
 		GridBagConstraints c8 = new GridBagConstraints();
-		c.gridx = 0;
-		c.gridy = 4;
-		c.anchor = GridBagConstraints.WEST;
+		c8.gridx = 0;
+		c8.gridy = 4;
+		c8.insets = new Insets(2, 2, 2, 2);
+		c8.anchor = GridBagConstraints.WEST;
 		getContentPane().add(brTelefona, c8);
 		
 		GridBagConstraints c9 = new GridBagConstraints();
-		c.gridx = 1;
-		c.gridy = 4;
+		c9.gridx = 1;
+		c9.gridy = 4;
+		c9.insets = new Insets(2, 2, 2, 2);
+		c9.fill = GridBagConstraints.HORIZONTAL;
 		getContentPane().add(brTelefonaT, c9);
 		
 		GridBagConstraints c10 = new GridBagConstraints();
-		c.gridx = 0;
-		c.gridy = 5;
-		c.anchor = GridBagConstraints.WEST;
+		c10.gridx = 0;
+		c10.gridy = 5;
+		c10.insets = new Insets(2, 2, 2, 2);
+		c10.anchor = GridBagConstraints.WEST;
 		getContentPane().add(brIndeksa, c10);
 		
 		GridBagConstraints c11 = new GridBagConstraints();
-		c.gridx = 1;
-		c.gridy = 5;
+		c11.gridx = 1;
+		c11.gridy = 5;
+		c11.insets = new Insets(2, 2, 2, 2);
+		c11.fill = GridBagConstraints.HORIZONTAL;
 		getContentPane().add(brIndeksaT, c11);
 		
 		GridBagConstraints c12 = new GridBagConstraints();
-		c.gridx = 0;
-		c.gridy = 6;
-		c.anchor = GridBagConstraints.WEST;
+		c12.gridx = 0;
+		c12.gridy = 6;
+		c12.insets = new Insets(2, 2, 2, 2);
+		c12.anchor = GridBagConstraints.WEST;
 		getContentPane().add(trenutnaGod, c12);
 		
 		GridBagConstraints c13 = new GridBagConstraints();
-		c.gridx = 1;
-		c.gridy = 6;
+		c13.gridx = 1;
+		c13.gridy = 6;
+		c13.fill = GridBagConstraints.HORIZONTAL;
+		c13.insets = new Insets(2, 2, 2, 2);
 		getContentPane().add(trenGod, c13);
 		
 		GridBagConstraints c14 = new GridBagConstraints();
-		c.gridx = 0;
-		c.gridy = 7;
-		c.anchor = GridBagConstraints.WEST;
+		c14.gridx = 0;
+		c14.gridy = 7;
+		c14.insets = new Insets(2, 2, 2, 2);
+		c14.anchor = GridBagConstraints.WEST;
 		getContentPane().add(radBtnB, c14);
 		
 		GridBagConstraints c15 = new GridBagConstraints();
-		c.gridx = 0;
-		c.gridy = 8;
-		c.anchor = GridBagConstraints.WEST;
+		c15.gridx = 0;
+		c15.gridy = 8;
+		c15.insets = new Insets(2, 2, 2, 2);
+		c15.anchor = GridBagConstraints.WEST;
 		getContentPane().add(radBtnS, c15);
+		
+		GridBagConstraints c16 = new GridBagConstraints();
+		c16.gridx = 1;
+		c16.gridy = 10;
+		c16.insets = new Insets(2, 2, 2, 2);
+		c16.anchor = GridBagConstraints.WEST;
+		getContentPane().add(otkazi, c16);
+		
+		GridBagConstraints c17 = new GridBagConstraints();
+		c17.gridx = 1;
+		c17.gridy = 10;
+		c17.insets = new Insets(2, 2, 2, 2);
+		c17.anchor = GridBagConstraints.EAST;
+		getContentPane().add(potvrdi, c17);
 		
 	}
 	
