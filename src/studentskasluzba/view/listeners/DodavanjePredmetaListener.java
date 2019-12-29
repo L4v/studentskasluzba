c70dc63eda1eba22847435afeb9a84fb71ba60f1
L4v <ivos.jovan@protonmail.ch>
2019-12-29 15:40:11 +0100

    #dodavanje_predmeta zapocet action listener

---
+++ src/studentskasluzba/view/listeners/DodavanjePredmetaListener.java
@@ -0,0 +1,14 @@
+package studentskasluzba.view.listeners;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class DodavanjePredmetaListener implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
