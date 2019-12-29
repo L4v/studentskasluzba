0a293e4865a04186bab43ccd197f7b1cb37e819e
L4v <ivos.jovan@protonmail.ch>
2019-12-29 15:11:28 +0100

    #uklanjanje_profesora uradjeno

--- src/studentskasluzba/controller/ProfesorController.java
+++ src/studentskasluzba/controller/ProfesorController.java
@@ -1,7 +1,10 @@
 package studentskasluzba.controller;
 
+import studentskasluzba.model.BazaPredmet;
 import studentskasluzba.model.BazaProfesor;
+import studentskasluzba.model.Predmet;
 import studentskasluzba.model.Profesor;
+import studentskasluzba.view.GlavniProzor;
 
 public class ProfesorController {
 	private static ProfesorController instance = null;
@@ -26,7 +29,20 @@ public class ProfesorController {
 	
 	public void removeProfesor(int row)
 	{
+		// NOTE(Jovan): Uklanjamo profesora sa svakog predmeta
+		Profesor prof = BazaProfesor.getInstance().getProfesor(row);
+		for(Predmet p : BazaPredmet.getInstance().getPredmete())
+		{
+			if(p.getProfesor().equals(prof)) 
+			{
+				p.removeProfesor();
+			}
+		}
+		
+		// NOTE(Jovan): Uklanjanje profesora i cuvanje podatak
 		BazaProfesor.getInstance().removeProfesor(row);
+		GlavniProzor.getInstance().azurirajPrikaz();
+		GlavniProzor.getInstance().saveAllDBs();
 	}
 	
 	public void editProfesor(Profesor p)
