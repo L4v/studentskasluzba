d21447d59edd37d451cc4f2851c2b220761a3e86
Kris98Wolf <57412501+Kris98Wolf@users.noreply.github.com>
2019-12-30 11:28:30 +0100

    #male_izmene

--- src/studentskasluzba/view/dijalog/AboutDialog.java
+++ src/studentskasluzba/view/dijalog/AboutDialog.java
@@ -1,6 +1,17 @@
 package studentskasluzba.view.dijalog;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class AboutDialog extends JDialog{
 
@@ -11,5 +22,64 @@ public class AboutDialog extends JDialog{
 
 	public AboutDialog() {
 		
+		setTitle("About");
+		setSize(650,550);
+		setLocationRelativeTo(null);
+		this.setModal(true);
+		
+		BorderLayout layout = new BorderLayout();
+		getContentPane().setLayout(layout);
+		
+		JPanel stud = new JPanel(new GridLayout(2,2));
+		JPanel bttn = new JPanel();
+		
+		ImageIcon icon=null;
+		try {
+			icon = new ImageIcon(ImageIO.read(getClass().getResource("/icons/student1.jpg")));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		JLabel tmp = new JLabel();
+		tmp.setIcon(icon);
+		
+		stud.add(tmp);
+		stud.add(new JLabel("heeej"));
+		
+		
+		ImageIcon icon2=null;
+		try {
+			icon2 = new ImageIcon(ImageIO.read(getClass().getResource("/icons/student2.jpg")));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		JLabel tmp2 = new JLabel();
+		tmp2.setIcon(icon2);
+		
+		stud.add(tmp2);
+		stud.add(new JLabel("heeej drugari!"));
+		
+		
+		
+		
+		
+		
+				
+		
+		JButton ok = new JButton("OK");
+		bttn.add(ok);
+		
+		ok.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				dispose();			
+			}
+		});
+		
+		this.add(bttn,BorderLayout.SOUTH);
+		this.add(stud,BorderLayout.CENTER);
 	}
 }
--- src/studentskasluzba/view/dijalog/HelpDialog.java
+++ src/studentskasluzba/view/dijalog/HelpDialog.java
@@ -1,6 +1,12 @@
 package studentskasluzba.view.dijalog;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JPanel;
 
 public class HelpDialog extends JDialog {
 
@@ -11,5 +17,27 @@ public class HelpDialog extends JDialog {
 
 	public HelpDialog() {
 		
+		setTitle("Help");
+		setSize(650,550);
+		setLocationRelativeTo(null);
+		this.setModal(true);
+		
+		BorderLayout layout = new BorderLayout();
+		getContentPane().setLayout(layout);
+		
+		JPanel bttn = new JPanel();
+		
+		
+		JButton ok = new JButton("OK");
+		bttn.add(ok);
+		
+		ok.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				dispose();			
+			}
+		});
+		
+		this.add(bttn,BorderLayout.SOUTH);
 	}
 }
