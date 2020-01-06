e3af11672de1c9205a016ef1b6a4ebddc3b77336
Kris98Wolf <57412501+Kris98Wolf@users.noreply.github.com>
2020-01-06 22:27:22 +0100

    #mali_dodaci

--- src/studentskasluzba/view/StudentTable.java
+++ src/studentskasluzba/view/StudentTable.java
@@ -29,7 +29,7 @@ public class StudentTable extends JTable{
 	}
 	
 	public void clearFilter() {
-		sorter.setRowFilter(RowFilter.regexFilter("", 0, 1));
+		sorter.setRowFilter(RowFilter.regexFilter("", 0, 1, 2));
 	}
 	
 	public void setFilter(String s, int i) {
